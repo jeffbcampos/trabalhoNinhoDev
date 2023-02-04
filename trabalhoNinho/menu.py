dcf5f48c7c01c4ead94606ce63440c9ab4d3f269
Jeferson Vieira Borges de Campos <jeffbcampos@gmail.com>
2023-02-04 09:56:18 -0300

    Add files via upload

---
+++ trabalhoNinho/menu.py
@@ -0,0 +1,57 @@
+from Controle.classConexao import Conexao
+from Modelo.classBiblioteca import Biblioteca
+
+
+
+def menu():
+    opcao = ''
+    while opcao != 5:
+        print("1 - Listar livros")
+        print("2 - Cadastrar livro")
+        print("3 - Atualizar livro")
+        print("4 - Deletar livro")
+        print("5 - Sair")
+
+        opcao = int(input("Digite a opção desejada: "))
+        
+        
+        match opcao:
+            case 1:
+                con = Conexao()
+                cursor = con.db.cursor()
+                biblioteca = Biblioteca(0, "", 0, 0, "")
+                biblioteca.listarLivro(cursor)
+                cursor.close()
+                con.db.close()
+                            
+            case 2:
+                con = Conexao()
+                cursor = con.db.cursor()
+                biblioteca = Biblioteca(0, "", 0, 0, "")
+                biblioteca.cadastrarLivro(cursor)
+                con.db.commit()
+                cursor.close()
+                con.db.close()
+                            
+            case 3:
+                con = Conexao()
+                cursor = con.db.cursor()
+                biblioteca = Biblioteca(0, "", 0, 0, "")
+                biblioteca.atualizarLivro(cursor)
+                con.db.commit()
+                cursor.close()
+                con.db.close()
+                            
+            case 4:
+                con = Conexao()
+                cursor = con.db.cursor()
+                biblioteca = Biblioteca(0, "", 0, 0, "")
+                biblioteca.deletarLivro(cursor)
+                con.db.commit()
+                cursor.close()
+                con.db.close()
+                            
+            case 5:
+                print("Saindo...")                        
+            case _:
+                print("Opção inválida")
